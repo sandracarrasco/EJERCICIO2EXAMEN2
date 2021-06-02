2aea9372ab08842f37fe6067aa06e5cb8b7199a4
SandraCalliconde <sady240299@gmail.com>
2021-06-01 22:02:38 -0400

    Add files via upload

---
+++ winejercicio1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace winejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ci = Convert.ToInt32(textBox1.Text);
+
+            ServiceReference1.WebService1SoapClient ws = new ServiceReference1.WebService1SoapClient();
+            dataGridView1.DataSource = ws.materia(ci).Tables[0];
+        }
+    }
+}
